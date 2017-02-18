17fc6419fa9c47fb58116bdd9cfda95bf0637071
kevin <khan1782@gmail.com>
2017-02-18 01:33:47 -0600

    added a moving star background

--- controllers.js
+++ controllers.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+//identify starfield div container
+var container = document.getElementById('stars-background');
+//initialize new starfield beep beep boop
+var starfield = new Starfield();
+//run initialize on initialized starfield
+starfield.initialize(container);
+starfield.start();
+
 
 
 });
